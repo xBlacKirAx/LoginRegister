9ec40fe1bed0654cd1d07170b82a628287cd5718
xblackirax <xblackirax@gmail.com>
2018-05-07 11:12:11 -0400

    防代签 php方面有问题

--- app/src/main/java/example/kira/loginregister/StudentAreaActivity.java
+++ app/src/main/java/example/kira/loginregister/StudentAreaActivity.java
@@ -40,8 +40,13 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.sql.SQLOutput;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class StudentAreaActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
@@ -51,32 +56,36 @@ public class StudentAreaActivity extends AppCompatActivity implements AdapterVie
     double latitude;
     double longtitude;
     private BroadcastReceiver broadcastReceiver;
+    private String fileName="file.txt";
+    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+    Date date = new Date();
+    String currentDate=dateFormat.format(date);
 
+    Button bTemp;
     @Override
     protected void onResume() {
         super.onResume();
-        if(broadcastReceiver==null){
-            broadcastReceiver=new BroadcastReceiver() {
+        if (broadcastReceiver == null) {
+            broadcastReceiver = new BroadcastReceiver() {
                 @Override
                 public void onReceive(Context context, Intent intent) {
-                    latitude=(double)intent.getExtras().get("latitude");
-                    longtitude=(double) intent.getExtras().get("longtitude");
-                    System.out.println("latitude: "+latitude);
-                    System.out.println("longtitude: "+longtitude);
+                    latitude = (double) intent.getExtras().get("latitude");
+                    longtitude = (double) intent.getExtras().get("longtitude");
+                    System.out.println("latitude: " + latitude);
+                    System.out.println("longtitude: " + longtitude);
                 }
             };
         }
-        registerReceiver(broadcastReceiver,new IntentFilter("location_update"));
+        registerReceiver(broadcastReceiver, new IntentFilter("location_update"));
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(broadcastReceiver!=null){
+        if (broadcastReceiver != null) {
             unregisterReceiver(broadcastReceiver);
         }
     }
-
 
 
     @SuppressLint("NewApi")
@@ -87,7 +96,7 @@ public class StudentAreaActivity extends AppCompatActivity implements AdapterVie
 
         final TextView welcomeMessage = findViewById(R.id.tvWelcomeMsg);
         final Spinner spClass = findViewById(R.id.spClass);
-        bSign=findViewById(R.id.bSign);
+        bSign = findViewById(R.id.bSign);
 
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
@@ -98,30 +107,42 @@ public class StudentAreaActivity extends AppCompatActivity implements AdapterVie
         String message = name + ", you are logged in as student";
         welcomeMessage.setText(message);
         if (!runtime_permissions()) {
-            Intent gpsIntent =new Intent(getApplicationContext(),GPS_Service.class);
+            Intent gpsIntent = new Intent(getApplicationContext(), GPS_Service.class);
             startService(gpsIntent);
 
         }
 
+        bTemp=findViewById(R.id.bTemp);
+        bTemp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clearFile();
+                System.out.println("file: "+readFile(fileName)+"end");
+            }
+        });
+
+
     }
-    private boolean runtime_permissions(){
+
+    private boolean runtime_permissions() {
         System.out.println(Build.VERSION.SDK_INT);
-        if(Build.VERSION.SDK_INT>=23&& ContextCompat.checkSelfPermission(this,Manifest.permission
-                .ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED&&ContextCompat.checkSelfPermission(this,Manifest.permission
-                .ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},100);
+        if (Build.VERSION.SDK_INT >= 23 && ContextCompat.checkSelfPermission(this, Manifest.permission
+                .ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission
+                .ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 100);
             return true;
         }
         return false;
     }
+
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        TextView myText=(TextView) view;
+        TextView myText = (TextView) view;
         System.out.println(i);
-        Toast.makeText(this,"You selected "+myText.getText(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "You selected " + myText.getText(), Toast.LENGTH_SHORT).show();
 
         if (!runtime_permissions()) {
-            Intent gpsIntent =new Intent(getApplicationContext(),GPS_Service.class);
+            Intent gpsIntent = new Intent(getApplicationContext(), GPS_Service.class);
             startService(gpsIntent);
             enable_signin(i);
         }
@@ -136,63 +157,95 @@ public class StudentAreaActivity extends AppCompatActivity implements AdapterVie
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode==100){
-            if(grantResults[0]==PackageManager.PERMISSION_GRANTED&&grantResults[1]==PackageManager.PERMISSION_GRANTED){
-                Intent gpsIntent =new Intent(getApplicationContext(),GPS_Service.class);
+        if (requestCode == 100) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                Intent gpsIntent = new Intent(getApplicationContext(), GPS_Service.class);
                 startService(gpsIntent);
-            }else{
+            } else {
                 runtime_permissions();
             }
         }
     }
 
     private void enable_signin(int i) {
-        final int index=i;
-        Intent intent=getIntent();
-        cIDList=intent.getStringArrayListExtra("cIDList");
-        cNameList=intent.getStringArrayListExtra("cNameList");
-        final String sID=intent.getStringExtra("sID");
+        final int index = i;
+        Intent intent = getIntent();
+        cIDList = intent.getStringArrayListExtra("cIDList");
+        cNameList = intent.getStringArrayListExtra("cNameList");
+        final String sID = intent.getStringExtra("sID");
+
+
         bSign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                AlertDialog.Builder builder=new AlertDialog.Builder(StudentAreaActivity.this);
-                LayoutInflater inflater=StudentAreaActivity.this.getLayoutInflater();
-                final View rootView=inflater.inflate(R.layout.dialog_student_enter_vcode,null);
+                AlertDialog.Builder builder = new AlertDialog.Builder(StudentAreaActivity.this);
+                LayoutInflater inflater = StudentAreaActivity.this.getLayoutInflater();
+                final View rootView = inflater.inflate(R.layout.dialog_student_enter_vcode, null);
 
 
                 builder.setView(rootView)
                         .setPositiveButton("Sign in!", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
-                                Intent gpsIntent =new Intent(getApplicationContext(),GPS_Service.class);
-                                startService(gpsIntent);
-                                final EditText verificationCode=rootView.findViewById(R.id.verificationCode);
-                                final String vCode=verificationCode.getText().toString();
-                                System.out.println(sID+" "+cIDList.get(index));
-                                System.out.println(vCode);
 
-                                Response.Listener<String> responseListener=new Response.Listener<String>() {
+                                Intent gpsIntent = new Intent(getApplicationContext(), GPS_Service.class);
+                                startService(gpsIntent);
+                                final EditText verificationCode = rootView.findViewById(R.id.verificationCode);
+                                final String vCode = verificationCode.getText().toString();
+
+
+
+
+                                Response.Listener<String> responseListener = new Response.Listener<String>() {
                                     @Override
                                     public void onResponse(String response) {
-                                        try{
-                                            JSONObject jsonResponse=new JSONObject(response);
-                                            String errorResponse=jsonResponse.getString("response");
+                                        try {
+                                            JSONObject jsonResponse = new JSONObject(response);
+                                            String errorResponse = jsonResponse.getString("response");
 
-                                                AlertDialog.Builder builder = new AlertDialog.Builder(StudentAreaActivity.this);
-                                                builder.setMessage(errorResponse)
-                                                        .setNegativeButton("Back",null)
-                                                        .create()
-                                                        .show();
+                                            AlertDialog.Builder builder = new AlertDialog.Builder(StudentAreaActivity.this);
+                                            builder.setMessage(errorResponse)
+                                                    .setNegativeButton("Back", null)
+                                                    .create()
+                                                    .show();
+                                            if(jsonResponse.getBoolean("success")){
 
-                                        }catch(JSONException e){
+                                                if(readFile(fileName).contains(cIDList.get(index)+currentDate+sID)){
+                                                    System.out.println(readFile(fileName).substring(readFile(fileName).lastIndexOf(currentDate)+10,readFile(fileName).lastIndexOf(currentDate)+16));
+                                                    String cheatSID=readFile(fileName).substring(readFile(fileName).lastIndexOf(currentDate)+10,readFile(fileName).lastIndexOf(currentDate)+16);
+                                                    Response.Listener<String> cheatListener=new Response.Listener<String>() {
+                                                        @Override
+                                                        public void onResponse(String cheatResponse) {
+                                                            try{
+                                                                JSONObject jsonCheatResponse = new JSONObject(cheatResponse);
+                                                                boolean errorResponse = jsonCheatResponse.getBoolean("success");
+                                                                System.out.println(errorResponse);
+                                                            }catch (JSONException e){
+                                                                e.printStackTrace();
+                                                                System.out.println(cheatResponse);
+                                                            }
+
+                                                        }
+                                                    };
+                                                    cheatRequest cheatRequest=new cheatRequest(sID, cIDList.get(index),cheatSID,cheatListener);
+                                                    RequestQueue queue = Volley.newRequestQueue(StudentAreaActivity.this);
+                                                    queue.add(cheatRequest);
+                                                }else{
+                                                    //正常sign in
+                                                    System.out.println("正常sign in");
+                                                    saveFile(fileName,cIDList.get(index)+currentDate+sID+"\n");
+                                                }
+
+                                            }
+                                        } catch (JSONException e) {
                                             e.printStackTrace();
                                             System.out.println(response);
                                         }
                                     }
                                 };
 
-                                StudentAreaRequest studentAreaRequest = new StudentAreaRequest(sID,cIDList.get(index),vCode, Double.toString(latitude), Double.toString(longtitude), responseListener);
+                                StudentAreaRequest studentAreaRequest = new StudentAreaRequest(sID, cIDList.get(index), vCode, Double.toString(latitude), Double.toString(longtitude), responseListener);
                                 System.out.println(latitude);
                                 System.out.println(longtitude);
                                 RequestQueue queue = Volley.newRequestQueue(StudentAreaActivity.this);
@@ -202,12 +255,46 @@ public class StudentAreaActivity extends AppCompatActivity implements AdapterVie
 //                                stopService(gpsIntent);
                             }
                         })
-                        .setNegativeButton("Back",null)
-                        .setTitle(cIDList.get(index)+" "+cNameList.get(index))
+                        .setNegativeButton("Back", null)
+                        .setTitle(cIDList.get(index) + " " + cNameList.get(index))
                         .create()
                         .show();
             }
         });
+    }
+    public void clearFile(){
+        try{
+            FileOutputStream fos=openFileOutput(fileName,Context.MODE_PRIVATE);
+            fos.write('\n');
+            fos.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void saveFile(String file, String text){
+        try{
+            FileOutputStream fos=openFileOutput(file, Context.MODE_APPEND);
+            fos.write(text.getBytes());
+
+            fos.close();
+            Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public String readFile(String file){
+        String text="";
+        try{
+            FileInputStream fis=openFileInput(file);
+            int size=fis.available();
+            byte[] buffer=new byte[size];
+            fis.read(buffer);
+            fis.close();
+            text =new String(buffer);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return text;
     }
 
 }
--- app/src/main/java/example/kira/loginregister/cheatRequest.java
+++ app/src/main/java/example/kira/loginregister/cheatRequest.java
@@ -7,18 +7,18 @@ import com.android.volley.toolbox.StringRequest;
 import java.util.HashMap;
 import java.util.Map;
 
-class StudentAreaRequest extends StringRequest {
-    private static final String STUDENT_AREA_REQUEST_URL="http://xzhou26.000webhostapp.com/StudentArea.php";
+public class cheatRequest extends StringRequest {
+    private static final String STUDENT_AREA_REQUEST_URL="http://xzhou26.000webhostapp.com/Cheat.php";
     private Map<String, String> params;
 
-    public StudentAreaRequest(String studentID, String classID, String verificationCode, String langtitude, String longtitude, Response.Listener<String> listener){
+    public cheatRequest(String studentID, String classID, String cheatSID, Response.Listener<String> listener){
         super(Request.Method.POST,STUDENT_AREA_REQUEST_URL,listener,null);
         params=new HashMap<>();
         params.put("sID",studentID);
         params.put("cID",classID);
-        params.put("verificationCode",verificationCode);
-        params.put("langtitude", langtitude);
-        params.put("longtitude",longtitude);
+        params.put("sID2",cheatSID);
+        params.put("cID2",classID);
+
     }
 
     @Override
