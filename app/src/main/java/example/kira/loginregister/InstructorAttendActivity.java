408441aa0a68db0b2f551bd928f021eaf16a6e5c
xblackirax <xblackirax@gmail.com>
2018-05-07 06:38:24 -0400

    继续优化了InstrucotrAttendActivity 删除了重复CODE和几个没用的BUTTON 加入了学生attendance status 剩余工作： 修正LOGIN界面输错用户名密码不提示的BUG 用PHP时间来当做老师生成随机验证码的时间 随机数生成器 防代签

--- app/src/main/java/example/kira/loginregister/InstructorAttendActivity.java
+++ app/src/main/java/example/kira/loginregister/InstructorAttendActivity.java
@@ -21,126 +21,100 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class InstructorAttendActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+public class InstructorAttendActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     String verificationCode = "xxaabb";
-    Button back;
+    Button bCheck;
     Button current;
     String cID = "";
     ArrayList<String> attendList = new ArrayList<>();
-    ArrayList<String> dateList=new ArrayList<>();
+    ArrayList<String> dateList = new ArrayList<>();
     TextView attend;
     String date = "";
+    Response.Listener<String> responseListener;
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendence);
-        back = findViewById(R.id.back);
-        current = findViewById(R.id.Current);
+        bCheck = findViewById(R.id.bCheck);
 
         final Spinner spDate = findViewById(R.id.spDate);
         attend = findViewById(R.id.attend);
         Intent intent = getIntent();
         cID = intent.getStringExtra("cIDa");
-        dateList=intent.getStringArrayListExtra("dateList");
-        ArrayAdapter<String> ada=new ArrayAdapter<>(this,R.layout.support_simple_spinner_dropdown_item,dateList);
+        dateList = intent.getStringArrayListExtra("dateList");
+        ArrayAdapter<String> ada = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, dateList);
         spDate.setAdapter(ada);
         spDate.setOnItemSelectedListener(this);
 
-        attend.setText("Please tap Current to show the attendance for today's class or select a date and tap show.");
-        current.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    final String currentDate=dateList.get(dateList.size()-1);
-                    attend.setText("");
-                    Response.Listener<String> responseListener = new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            try {
-                                JSONObject json = new JSONObject(response);
-                                JSONObject jsonResponse = new JSONObject(json.getString("response"));
-                                boolean success = jsonResponse.getBoolean("success");
-                                if (success) {
-                                    JSONObject jsonAID = new JSONObject(json.getString("aID"));
-                                    convertJSONObjectToArrary(jsonAID);
-                                    attend.append(currentDate+"\n");
-                                    for (int j = attendList.size() - 1; j >= 0; j--) {
-                                        attend.append(attendList.get(j) + "\n");
-                                    }
-                                    System.out.print(attend);
 
-                                }
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                                System.out.println(response);
-                            }
-                        }
-                    };
-                    InstructorGetAttendanceRequest instructorGetAttendanceRequest = new InstructorGetAttendanceRequest(cID, currentDate, responseListener);
-                    RequestQueue queue = Volley.newRequestQueue(InstructorAttendActivity.this);
-                    queue.add(instructorGetAttendanceRequest);
+        final String currentDate = dateList.get(dateList.size() - 1);
+        attend.setText("");
+        attend.append(currentDate + "\n");
+        responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject json = new JSONObject(response);
+                    JSONObject jsonAID = new JSONObject(json.getString("totalStudentOfClass"));
+                    convertJSONObjectToArrary(jsonAID);
+
+                    for (int j = attendList.size() - 1; j >= 0; j--) {
+                        attend.append(attendList.get(j) + "\n");
+                    }
+                    System.out.print(attend);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    System.out.println(response);
                 }
-            });
-
-        }
-        @Override
-        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                TextView myText=(TextView) view;
-                System.out.println(i);
-                date = dateList.get(i);
-                Toast.makeText(adapterView.getContext(),myText.getText(), Toast.LENGTH_SHORT).show();
-                back.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    attend.setText("");
-                    Response.Listener<String> responseListener = new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            try {
-                                JSONObject json = new JSONObject(response);
-                                JSONObject jsonResponse = new JSONObject(json.getString("response"));
-                                boolean success = jsonResponse.getBoolean("success");
-                                if (success) {
-                                    JSONObject jsonAID = new JSONObject(json.getString("aID"));
-                                    convertJSONObjectToArrary(jsonAID);
-                                    attend.append(date+"\n");
-                                    for (int j = attendList.size() - 1; j >= 0; j--) {
-                                        attend.append(attendList.get(j) + "\n");
-                                    }
-                                    System.out.print(attend);
-
-                                }
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                                System.out.println(response);
-                            }
-                        }
-                    };
-                    InstructorGetAttendanceRequest instructorGetAttendanceRequest = new InstructorGetAttendanceRequest(cID, date, responseListener);
-                    RequestQueue queue = Volley.newRequestQueue(InstructorAttendActivity.this);
-                    queue.add(instructorGetAttendanceRequest);
-                }
-            });
-        }
-        public void onNothingSelected(AdapterView<?> adapterView) {
-
-        }
-        protected void convertJSONObjectToArrary (JSONObject aID){
-            Iterator x = aID.keys();
-            attendList.clear();
-            try {
-                while (x.hasNext()) {
-                    String key = (String) x.next();
-                    attendList.add(aID.get(key).toString());
-                }
-
-            } catch (JSONException e) {
-                e.printStackTrace();
             }
+        };
+        InstructorGetAttendanceRequest instructorGetAttendanceRequest = new InstructorGetAttendanceRequest(cID, currentDate, responseListener);
+        RequestQueue queue = Volley.newRequestQueue(InstructorAttendActivity.this);
+        queue.add(instructorGetAttendanceRequest);
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        TextView myText = (TextView) view;
+        System.out.println(i);
+        date = dateList.get(i);
+        Toast.makeText(adapterView.getContext(), myText.getText(), Toast.LENGTH_SHORT).show();
+        bCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attend.setText("");
+                attend.append(date + "\n");
+                InstructorGetAttendanceRequest instructorGetAttendanceRequest = new InstructorGetAttendanceRequest(cID, date, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(InstructorAttendActivity.this);
+                queue.add(instructorGetAttendanceRequest);
+            }
+        });
+    }
+
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    protected void convertJSONObjectToArrary(JSONObject aID) {
+        Iterator x = aID.keys();
+        attendList.clear();
+        try {
+            while (x.hasNext()) {
+                String key = (String) x.next();
+                attendList.add(aID.get(key).toString());
+                System.out.println(aID.get(key).toString());
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
-
-
-
+    }
 
 
 }
